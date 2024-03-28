ff36abd7e731a88cf116e3e15663b339e749ecd3
Chun-Hui Gao <gaospecial@gmail.com>
2024-03-28 17:33:09 +0800

    add runApp()

---
+++ R/runApp.R
@@ -0,0 +1,22 @@
+#' Launch Reactor Data Shiny App
+#'
+#' @return a shiny app
+#' @export
+runApp = function(){
+  check_package("shiny")
+  app = system.file(
+    "shiny",
+    "shinyApp.R",
+    package = "syncons",
+    mustWork = TRUE
+  )
+  if(interactive()) shiny::runApp(app)
+}
+
+check_package = function(package){
+  if (!requireNamespace(package, quietly = TRUE)){
+    stop(glue::glue("The '{package}' package is required but not installed.",
+               "\tPlease run `install.packages('{package}')` and retry...",
+               .sep = "\n"))
+  }
+}
