d49bed4d52195dd6aa6ddd37b96c6a87fb46267c
Chun-Hui Gao <gaospecial@gmail.com>
2024-03-28 17:16:21 +0800

    simple table

--- R/table.R
+++ R/table.R
@@ -1,21 +1,18 @@
-var2header = function(x){
-  gsub("_"," ",x)
-}
+
 
 # a DT table
 DT_table = function(df){
   df |>
     DT::datatable(extensions = c("Buttons"),
-                  colnames = var2header(colnames(.)),
                   rownames = FALSE,
                   options = list(
                     dom = 'Bfrtip',
                     scrollX = TRUE,
                     buttons = list(
                       'pageLength',
-                      list(extend="excel",
-                           filename=paste0(substitute(df)),
-                           header=TRUE)
+                      list(extend = "excel",
+                           filename = paste0(substitute(df)),
+                           header = TRUE)
                     )
                   ))
 }
