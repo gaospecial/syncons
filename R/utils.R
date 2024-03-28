0d9850d5ec3dc9e2a4c52abd678b6bcd04c40aad
Chun-Hui Gao <gaospecial@gmail.com>
2024-03-28 17:32:58 +0800

    complete plate functions

--- R/utils.R
+++ R/utils.R
@@ -14,30 +14,80 @@
 ## (PART) Experimental Table Generation
 #######################################
 
-#' The composition of community in a 8x8 plate (64 SynComs)
+
+#' Assign multiple strains to one or more specified plates
+#'
+#' @param strains a character vector specifing the strain names
+#' @param strain_sep separator used to join strains into combination
+#' @param plate_type specify plate type, either 24, 96, or 384.
+#' @param plate_prefix plate prefix
+#' @param return_layout return the layout of plate, instead of a sample table if TRUE
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'   assign_plate(letters[1:5])
+assign_plate = function(strains,
+                        strain_sep = "/",
+                        plate_type = c("24","96","384"),
+                        plate_prefix = "P",
+                        return_layout = FALSE){
+  if (!is.character(plate_type)) plate_type = as.character(plate_type)
+  plate_type = match.arg(plate_type)
+  nstrain = length(strains)
+  valid_plate_types = valid_plate_type(nstrain)
+  if (!(plate_type %in% valid_plate_types)) {
+    warning("Plate type is not valid, resetting to ", valid_plate_types[[length(valid_plate_types)]])
+    plate_type = valid_plate_types[[length(valid_plate_types)]]
+  }
+  n_plate_max = switch(plate_type, "24" = 4, "96" = 6, "384" = 8)
+  strain_add = strains[1:n_plate_max]
+  strain_bases = strain_combination(strains = strains[-(1:n_plate_max)], strain_sep = strain_sep)
+  plates = lapply(seq_along(strain_bases), function(i){
+      one_plate(plate_type = plate_type,
+                plate_name = paste0(plate_prefix, i),
+                strain_base = strain_bases[[i]],
+                strain_add = strain_add,
+                strain_sep = strain_sep,
+                return_layout = return_layout)
+  })
+  table = dplyr::bind_rows(plates)
+  return(table)
+}
+
+valid_plate_type = function(nstrain){
+  if (nstrain < 4 | nstrain > 11) stop("Applicable nubmer of strains is from 4 to 11.")
+  if (nstrain >= 8) return(c("24","96","384"))
+  if (nstrain >= 6) return(c("24","96"))
+  return("24")
+}
+
+#' The composition of community in a single 24-, 96- or 384-well plate
 #'
 #' We use a fast, extensible method to generate multiple SynComs in every
-#' possible composition. The smallest unit is a 8x8 plate with 64 SynComs,
-#' which are all the combinations of six different strains. Based on this,
+#' possible composition. The smallest unit is either a 4 x 4 plate (24 well)
+#' for 16 SynComs, a 8x8 plate (96 well) for 64 SynComs, or a 16 x 16
+#' plate (384 well) for 256 SynComs. Based on this,
 #' one can generate as many as plates, and in that cases the combination
-#' of SynComs could be 64x2, 64x2x2, 64x2x2, and so on. For each time we
+#' of SynComs could be multiplied in batch. For each time we
 #' add an extra strain, the number of total plates will double to fulfill
 #' all additional strain combinations. In other words, the possible
 #' combination of $n$ strain equals to $2^n$.
 #'
 #' @param plate_type specify plate type, either 24, 96, or 384.
-#' @param plate_name a prefix in combination id
+#' @param plate_name will be used as the prefix in combination id
 #' @param strain_base the base strain or community in a single plate
-#' @param strain_add the id of added strains, which should contains 6 members
-#' @param dim c(8,8)
-#' @param return_array return an array if TRUE
-#' @param strain_seq separator
+#' @param strain_add the id of added strains
+#' @param strain_sep separator used to join strains into combination
+#' @param return_layout return the layout of plate, instead of a sample table if TRUE
 #'
-#' @return a tibble or an array
+#' @return a tibble
 #' @export
+#' @md
 #'
 #' @examples
-#'   one_plate(plate_name="",strain_base="",strain_add=LETTERS[1:6])
+#'   one_plate(plate_name = "", strain_base= "X/Y", strain_add = LETTERS[1:6])
 one_plate = function(plate_type = c("24","96","384"),
                      plate_name = NULL,
                      strain_base = NULL,
@@ -52,8 +102,7 @@ one_plate = function(plate_type = c("24","96","384"),
   if (2^length(strain_add) > n_well) {
     stop(glue::glue("A {plate_type}-well plate meets with {log2(n_well)} strains.",
                     "Please relocate these strains to more plates", .sep = "\n"))
-  }
-  if (2^length(strain_add) < n_well) {
+  } else if (2^length(strain_add) < n_well) {
     stop(glue::glue("A {plate_type}-well plate meets with {log2(n_well)} strains.",
                     "Please select a small plate and try again.", .sep = "\n"))
   }
@@ -78,19 +127,13 @@ one_plate = function(plate_type = c("24","96","384"),
       dplyr::mutate(row = sub("[0-9]+","", well), col = sub('[A-Z]+',"", well)) |>
       dplyr::select(row, col, combination) |>
       tidyr::pivot_wider(names_from = col, values_from = combination) |>
-      tibble::column_to_rownames(var = "row")
+      dplyr::mutate(row = paste0(plate_name, row))
   } else {
     data = data |> dplyr::select(-well)
   }
 
   return(data)
 }
-
-
-#######################################
-## (PART) Combination Relationship
-#######################################
-
 
 #' Sort combination in a string
 #'
@@ -112,20 +155,34 @@ sort_combination = function(strain_combination, strain_sep = "/"){
   l = lapply(strain_combination, function(s){
     stringr::str_split(s, strain_sep, simplify = TRUE) |>
       stringr::str_sort(numeric = TRUE) |>
-      paste0(collapse = sep)
+      paste0(collapse = strain_sep)
   })
   unlist(l)
 }
 
 
+#' All the possible combination of multiple strains
+#'
+#' @param strains names of strains
+#' @param strain_sep
+#'
+#' @return
+#' @export
+#'
+#' @examples
+strain_combination = function(strains, strain_sep = "/"){
+  comb_id = combinations(length(strains))
+  comb_str = sapply(comb_id, function(i) strains[i])
+  combinations = sapply(comb_str, function(i) paste0(i, collapse = strain_sep))
+  return(combinations)
+}
+
 #' all possible combinations of n sets
 #'
 #' @param n dim
-#'
-#' @importFrom utils combn
 combinations = function(n) {
-  l = lapply(seq_len(n), function(x){
-    m = combn(n,x)
+  l = lapply(0:n, function(x){
+    m = utils::combn(n,x)
     matrix2list(m)
   })
   unlist(l, recursive = F)
