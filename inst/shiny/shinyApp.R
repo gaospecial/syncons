e168daeb350346c82fd9c0427c5431f7917e8431
Chun-Hui Gao <gaospecial@gmail.com>
2024-03-29 10:21:18 +0800

    repharse tips

--- inst/shiny/shinyApp.R
+++ inst/shiny/shinyApp.R
@@ -42,15 +42,16 @@ server = function(input, output, session) {
   observeEvent(input$strain_name, {
     strain_name = input$strain_name |> strsplit(split = "[\n\r]+") |> unlist()
     if (length(strain_name) < 4 | length(strain_name) > 11){
-      showNotification("Error: Applicable number of strains is from 4 to 11. Please edit your strain names.", duration = NULL, type = "error", id = "strain-name-notification")
+      showNotification("Error: Applicable number of strains is from 4 to 11. Please edit your strain names.", duration = 5, type = "error", id = "strain-name-notification")
       return()
     }
     updateRadioButtons(inputId = "plate_type", choices = syncons:::valid_plate_type(length(strain_name)))
-    showNotification("Notice: only applicable plate types are shown.", duration = NULL, type = "warning", id = "strain-name-notification")
+    showNotification("Notice: only applicable plate types are shown.", duration = 5, type = "warning", id = "strain-name-notification")
   })
 
   observeEvent(input$plate_type, {
     # 当 plate_type 发生变化时，显示加样顺序
+    output$message = renderPrint(markdown("**Note**: Please check the following diagram to verify the sequence that would be used to make strain combinations, as you have changed plate type recently."))
     output$diagram = renderUI(img(src = paste0(input$plate_type, "-well.png"), width = "100%"))
     output$table = renderUI("")
   })
@@ -61,8 +62,7 @@ server = function(input, output, session) {
     strain_name = input$strain_name |> strsplit(split = "[\n\r]+") |> unlist()
 
     # 处理 message
-    output$message = renderText("The following table lists all the combination id and composition of your SynComs.
-                                You may view, sort, search or export to Excel as you wish.")
+    output$message = renderPrint(markdown("**Notes**: The following table lists all the combination id and composition of your SynComs. You may *view*, *sort*, *search* or export to *Excel* as you wish."))
     removeNotification("strain-name-notification")
     output$diagram = renderUI("")
 
